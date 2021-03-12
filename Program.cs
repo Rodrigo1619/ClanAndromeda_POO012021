40393996506e25da0ae6e651d84e8fa3edf75955
rodrigo1619 <00067920@uca.edu.sv>
2021-03-11 20:46:18 -0600

    first phase of the program with the sum

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ActIntroGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            Console.WriteLine("Type a value for a: ");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Type a value for b: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Adding {a} + {b} equals {AddNumbers(a, b)}");
+        }
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
